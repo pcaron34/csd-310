f8915d8786cd50d4fd83afb7d46cc77f17f5124f
Paul Caron <97402731+pcaron34@users.noreply.github.com>
2022-04-05 19:58:05 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,14 +1,23 @@
+# import mongoclient from pymongo
 from pymongo import MongoClient
 
+# store mongoDB connection url in a variable named url
 url = "mongodb+srv://admin:admin@cluster0.lk4hy.mongodb.net/students?retryWrites=true&w=majority"
 
+# assign MongoClient(url) to variable client
 client = MongoClient(url)
 
+# set client.pytech to db
 db = client.pytech
 
+# print out what was stated for deliverables
 print("-- Pytech COllection List --")
+
+# print the required info from the server with an added filter
+# to get all the gobblygoock out of what was supposed to be printed
 print(db.list_collection_names(filter=None))
 
+# makes the enter key exit the program
 a = input("End of program, press Enter key to exit...")
 if a:
     exit(0) 
