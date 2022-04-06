64b3583498b5b418e3708f79f6883c659d739809
Paul Caron <97402731+pcaron34@users.noreply.github.com>
2022-04-05 19:23:48 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,6 +1,18 @@
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.lk4hy.mongodb.net/students?retryWrites=true&w=majority"
+
 client = MongoClient(url)
+
 db = client.pytech
-print(db.list_collection_names)
+
+print("-- Pytech COllection List --")
+print(db.list_collection_names(filter=None))
+
+a = input("End of program, press Enter key to exit...")
+if a:
+    exit(0) 
+
+
+
+
