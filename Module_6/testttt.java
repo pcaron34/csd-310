1698bee93ba138a94350742ef46f4adc2d000f1d
Paul Caron <pcaron@my365.bellevue.edu>
2022-04-25 20:18:50 -0500

    yeah

---
+++ Module_6/testttt.java
@@ -0,0 +1,12 @@
+package Module_6;
+
+public class testttt {
+    for (int i = 1; i <= 6; i++) { 
+
+        for (int j = 6; j >= 1; j--){
+        
+          System.out.print(j <= i ? j + " " : " " + " "); 
+          System.out.println();
+        }
+      }
+    }}
---
+++ Module_9/pysports_join_queries.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+db = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "BigBadWolfEater9/er",
+    database = "pysports"
+)
+
+cursor = db.cursor()
+
+print('-- DISPLAYING PLAYER RECORDS --')
+cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id ORDER BY player_id ASC")
+player = cursor.fetchall()
+for row in player:
+    print("Player ID: ", row[0])
+    print("First Name: ", row[1])
+    print("Last Name: ", row[2])
+    print("Team Name: ", row[3])
+    print()
+
+
+input("\n\n Press any key to continue...")
+
